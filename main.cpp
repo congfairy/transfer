41679702e2f253031625fe70f65811faad4c3a28
root <root@datafactor.ihep.ac.cn>
2017-07-10 17:46:14 +0800

    client with pos

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main(int argc,char *argv[])
    if(strcmp(command,"read")==0)
    {
    //transread("202.122.37.90:28003","/root/leaf/pytoc/upload/night.mkv","/root/leaf/","0","0","0","1000000000000");
-   transread("202.122.37.90:28001","/home/wangcong/leaf/upload/night.mkv","/root/leaf/","0","0","0","1000000000000");
+   transread("202.122.37.90:28001","/root/leaf/pytoc/upload/test.log","/root/leaf/test.log","0","0","0","2097152");
    }
    else if(strcmp(command,"upload")==0)
    {
