9daefa9008858874070a2107e83650846e959d00
XuQi0207 <812577920@qq.com>
2017-08-22 17:22:01 +0800

    new bitmap lib

--- pycall.c
+++ pycall.c
@@ -1,7 +1,12 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include</usr/include/mysql/mysql.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <mysql/mysql.h>
+
+#define UNIT_SIZE (1024*1024)
+
+MYSQL *l_mysql;
 
 int splitname(char *path, char *basename){
         char *p;
@@ -21,92 +26,98 @@ int splitname(char *path, char *basename){
         return (0);
 }
 
-int update_bitmap(int ini_bit, int size, char *filename)
+int sql_init(){
+	FILE *mysql_config=fopen("/etc/NSCONFIG", "r");
+	char *p_user, *p_passwd, *p_server;
+	if(mysql_config==NULL){
+		printf("DBConfig: %s\n",strerror(ENOENT ));
+		return ENOENT ;
+	}
+	fseek(mysql_config, 0, SEEK_END);
+	int len=ftell(mysql_config);
+	fseek(mysql_config, 0, SEEK_SET);
+	char *buf=(char *)malloc(len*sizeof(char));
+	fgets(buf, len+1, mysql_config);
+	fclose(mysql_config);
+	if( strlen(buf)>=5 && (p_user=strtok(buf,"/\n")) && (p_passwd=strtok(NULL, "@\n")) && (p_server=strtok(NULL, "\n")) ){
+                
+                l_mysql=mysql_init(NULL);
+                if(l_mysql==NULL){
+                 	printf("mysql init failed: %s\n",strerror(EPIPE));
+			return EPIPE ;
+		}
+                if(NULL==(l_mysql=mysql_real_connect(l_mysql,p_server,p_user,p_passwd,"cns_db",0,NULL,0))){
+                	 printf("mysql connection: %s\n",strerror( ECONNREFUSED));
+			 return ECONNREFUSED;
+		}
+
+	}else{
+		printf("DBConfig: %s\n",strerror(EINVAL));
+		return EINVAL;
+	}
+	free(buf);
+	return 0;
+}
+
+int sql_close(){
+	mysql_close(l_mysql);
+	return 0;
+}
+
+int update_bitmap(off_t offset, size_t size, char *path, int filesize)
 {
-	int pro_bit=ini_bit/10;	
-	int size_bit=size/10;
-	int i;
-	printf("Data block start at %dMB, size is %dMB (Block unit is 10MB)\n",ini_bit,size);
-	if(size%10!=0){
-		size_bit++;
-	}	
-	char *bitmap=(char *)malloc(pro_bit+size_bit+1);
-	if(bitmap==NULL){
-		exit(1);
+	if(offset<0||size<=0||!path||filesize<=0){
+		printf("%s\n",strerror(EINVAL));
+		return  EINVAL;
 	}
-	memset(bitmap,'0',sizeof(char)*(pro_bit+size_bit));
-	for(i=pro_bit;i<pro_bit+size_bit;i++){
-		bitmap[i]='1';
+	int sblock_num=offset/UNIT_SIZE;
+	int eblock_num=(offset+size)/UNIT_SIZE;
+	if((offset+size)%UNIT_SIZE!=0){
+		eblock_num+=1;
 	}
-	printf("bitmap: %s\n",bitmap);
-	int pathlen=strlen(filename);
-	char *name=(char *)malloc(pathlen+1);
-	splitname(filename,name);
-	FILE *cf=fopen("DBConfig","r");
-	char *p_u, *p_p, *p_s;
-	if(cf==NULL){
-		printf("No DBConfig file\n");
-		exit(0);
-	}
-	fseek(cf,0,SEEK_END);
-        int len=ftell(cf);
-	fseek(cf,0,SEEK_SET);
-	char *buf=(char *)malloc(len*sizeof(char)+1);
-        fgets(buf,len+1,cf);
-	if(strlen(buf)>=5&&(p_u=strtok(buf,"/\n"))&&(p_p=strtok(NULL,"@\n"))&&(p_s=strtok(NULL,"\n"))){
-      		printf("%s  %s  %s\n",p_s, p_u, p_p);
-		fclose(cf);
-		MYSQL *t_mysql;
-		MYSQL_ROW row; 
+//	printf("offset=%d  size=%d  sblcok_num=%d  e_block_num=%d  UNIT_SIZE=%d  path=%s\n", offset, size, sblock_num, eblock_num, UNIT_SIZE, path);
+	int res=0;
+	if((res=sql_init())==0){
+		MYSQL_ROW row;
 		MYSQL_RES *result;
 		unsigned long *length;
-		char *check="select bitmap from Cns_file_transform_metadata where path='%s' and name='%s'";
-	       	char *bitmap_update="update Cns_file_transform_metadata set bitmap='%s' where path='%s' and name='%s'";
-     		char sql_check[1024];
+                char bitmap_update[]="update Cns_file_transform_metadata set bitmap='%s' where path='%s' and name='%s'";
 		char sql_update[1024];
-		sprintf(sql_check,check,filename,name);
-		printf("sql_check is %s\n",sql_check);
-		t_mysql=mysql_init(NULL);
-        	if(t_mysql==NULL){
-                	printf("mysql connection inition failed\n");
-                	exit(1);
-        	}
-      		if(NULL==mysql_real_connect(t_mysql,p_s,p_u,p_p,"cns_db",0,NULL,0)){
-               		printf("mysql connection failed\n");
-                	exit(1);
-        	}else
-               	        printf("mysql connect successfully\n");	
-		mysql_real_query(t_mysql,sql_check,strlen(sql_check));
-		result=mysql_store_result(t_mysql);
-	        row=mysql_fetch_row(result);
+
+                char sql_check[1024];
+                char *basename=(char *)malloc(strlen(path)+1);
+                char check[]="select bitmap from Cns_file_transform_metadata where path='%s' and name='%s'";
+                splitname(path, basename);
+                sprintf(sql_check, check, path, basename);
+                if(mysql_real_query(l_mysql,sql_check,strlen(sql_check))!=0){
+                        printf("select failed no such file\n");
+                        return ENOEXEC;
+                }
+
+                result=mysql_store_result(l_mysql);
+                row=mysql_fetch_row(result);
 		length=mysql_fetch_lengths(result);
-		if(length[0]!=0){
-			int max=strlen(bitmap)>length[0]?strlen(bitmap):length[0];
-			int temp;
-			char *bitmap_new=(char *)malloc(max+1);
-			for(temp=0;temp<max;temp++){
-				if(bitmap[temp]=='1'||row[0][temp]=='1'){
-					bitmap_new[temp]='1';
-				}else
-					bitmap_new[temp]='0';
-			}
-			bitmap_new[max]='\0';	
-			sprintf(sql_update,bitmap_update,bitmap_new,filename,name);
+		if(length[0]==0){
+			printf("Bitmap: %s\n", strerror(ENODATA));
+			return ENODATA;
 		}else{
-			sprintf(sql_update,bitmap_update,bitmap,filename,name);
+			int i;
+			char *bitmap=(char *)malloc((length[0]+1)*sizeof(char));
+			strcpy(bitmap, row[0]);
+			for(i=sblock_num;i<eblock_num;i++){
+				bitmap[i]='1';
+			}
+			sprintf(sql_update, bitmap_update, bitmap, path, basename);
+			mysql_free_result(result);
+			free(bitmap);
+			free(basename);
+//			printf("sql_for_update: %s\n",sql_update);
+			if(mysql_real_query(l_mysql, sql_update, strlen(sql_update))!=0){
+				printf("sql excute failed\n");
+				return ENOEXEC;
+			}
 		}
-		printf("update_sql is %s\n",sql_update);
-        	int a;
-        	if(a=mysql_real_query(t_mysql,sql_update,strlen(sql_update))!=0){
-                	printf("bitmap_update failed\n");
-			mysql_close(t_mysql);
-                	return 1;
-        	}else
-                	printf("bitmap_update successfully\n");
-			mysql_close(t_mysql);
-			return 0;
-	}else{
-		printf("ERROR in DBCONFIG\n");
-		return 1;
 	}
+	sql_close();
+	return res;
 }
--- pycall.py
+++ pycall.py
@@ -1,16 +1,5 @@
-from ctypes import *
-import os
-
-
-#ll=ctypes.cdll.LoadLibrary
-#lib=ll('./libpycall.so')
-#p=lib.foo(1,3)
-#lib.update_bitmap(52,111,ctypes.c_char_p("/root/leaf/night.mkv"))
-#print "result=%d"%p
-#print("finish")
-lib=cdll.LoadLibrary('./libpycall1.so')
-print("start")
-func=lib.update_bitmap
-func.argtypes=(c_int,c_int,c_char_p)
-func=lib.update_bitmap(52,111,"/root/leaf/pytoc/upload/night.mkv".encode("utf-8"))
-print("finish")
+import ctypes
+ll=ctypes.cdll.LoadLibrary
+lib=ll("./libpycall.so")
+lib.update_bitmap(1048576, 2097152, "/root/leaf/pytoc/upload/test.log", 629146148)
+print ("finish")
